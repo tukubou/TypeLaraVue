417fd773012c07214edda82144cf1071b6106ffe
Yuta-Shimogishi <tukubou417@gmail.com>
2019-03-31 18:07:25 +0900

    サーバで動かすためにApplication.phpを追加

---
+++ app/Application.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App;
+
+class Application extends \Illuminate\Foundation\Application
+{
+    public function publicPath()
+    {
+      return '/home/albarista/tukubou.com/TypeLaraVue/public';
+    }
+}
