550208b5683e0b1bf73e9667cf66a5910aa45c77
Yuta-Shimogishi <tukubou417@gmail.com>
2019-03-31 17:57:16 +0900

    public/indexをアプリがサーバで動くように修正

--- public/index.php
+++ public/index.php
@@ -21,8 +21,8 @@ define('LARAVEL_START', microtime(true));
 |
 */
 
-require __DIR__.'/../vendor/autoload.php';
-
+// require __DIR__.'/../vendor/autoload.php';
+require '/home/albarista/tukubou.com/TypeLaraVue/vendor/autoload.php';
 /*
 |--------------------------------------------------------------------------
 | Turn On The Lights
@@ -35,7 +35,8 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-$app = require_once __DIR__.'/../bootstrap/app.php';
+// $app = require_once __DIR__.'/../bootstrap/app.php';
+$app = require_once '/home/albarista/tukubou.com/TypeLaraVue/bootstrap/app.php';
 
 /*
 |--------------------------------------------------------------------------
