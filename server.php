556a7c4a85f6a8f47a8c731fc133f12e22849cdb
Yuta-Shimogishi <tukubou417@gmail.com>
2019-03-31 18:00:51 +0900

    server.phpをサーバで動くように修正

--- server.php
+++ server.php
@@ -14,8 +14,13 @@ $uri = urldecode(
 // This file allows us to emulate Apache's "mod_rewrite" functionality from the
 // built-in PHP web server. This provides a convenient way to test a Laravel
 // application without having installed a "real" web server software here.
-if ($uri !== '/' && file_exists(__DIR__.'/public'.$uri)) {
+// ローカルで実行する場合
+$public_path = __DIR__.'/public';
+// リモートで実行する場合
+$public_path = '/home/albarista/tukubou.com/TypeLaraVue/public';
+$file = $public_path.$uri;
+if ($uri !== '/' && file_exists($file)) {
     return false;
 }
 
-require_once __DIR__.'/public/index.php';
+require_once $public_path.'/index.php';
