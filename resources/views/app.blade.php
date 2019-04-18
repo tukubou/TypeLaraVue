17b352745ff72f4491d3cf13bd62d0b646ebd5bc
Yuta-Shimogishi <tukubou417@gmail.com>
2019-04-19 03:01:47 +0900

    Viewのおおもと修正

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,23 +1,21 @@
 <!DOCTYPE html>
 <html lang="{{ app()->getLocale() }}">
-
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="csrf-token" content="{{ csrf_token() }}">
-  <title>Laravel</title>
-  <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+  <title>TECHREIS</title>
 </head>
-
 <body>
   <div id="app" class="container-fluid p-0">
       <!-- 共通 Header -->
       <vue-header></vue-header>
       <!-- ルートごとに変わるコンテンツ -->
       <router-view></router-view>
+      <!-- 共通 Footer -->
+      <vue-footer></vue-footer>
   </div>
 </body>
 <script src="{{ mix('js/app.js') }}"></script>
-
 </html>
